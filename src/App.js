e2f4fa715c35fae335fe859c641bcbbeab7acef3
makarand172 <42333028+makarand172@users.noreply.github.com>
2025-05-16 11:06:39 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [input, setInput] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  const [cache, setCache] = useState({});
+
+  const fetchData = async () => {
+    if (cache[input]) {
+      console.log("cache called" + input);
+      setRecipes(cache[input]);
+      return;
+    }
+
+    console.log("API Called" + input);
+    const response = await fetch(
+      "https://dummyjson.com/recipes/search?q=" + input
+    );
+    const results = await response.json();
+    setCache((prev) => ({ ...prev, [input]: results }));
+    setRecipes(results.recipes);
+  };
+
+  useEffect(() => {
+    let timer = setTimeout(fetchData, 400);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
+
+  return (
+    <div className="App">
+      <h1>Auto Search Complete</h1>
+      <input
+        className="search-input"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      {recipes.length > 0 && (
+        <div className="res-container">
+          {recipes.map((res) => (
+            <span key={res.id} className="search-results">
+              {res.name}
+            </span>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
